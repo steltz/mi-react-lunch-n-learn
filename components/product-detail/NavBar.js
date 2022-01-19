bdda19d0c04c2a2401147e9a01dfa2cc069724e2
Nic Stelter <nicholas.stelter@gmail.com>
2022-01-19 09:51:05 -0500

    removed finished components

--- components/product-detail/NavBar.js
+++
@@ -1,7 +0,0 @@
-import styles from "../../styles/ProductDetail.module.css"
-
-const NavBar = () => {
-    return (<div className={styles.navBarContainer}>Bonobos</div>)
-}
-
-export default NavBar;
--- components/product-detail/ProductImage.js
+++
@@ -1,7 +0,0 @@
-import styles from "../../styles/ProductDetail.module.css";
-
-const ProductImage = (props) => {
-    return (<img className={styles.productImage} {...props} />)
-}
-
-export default ProductImage;
