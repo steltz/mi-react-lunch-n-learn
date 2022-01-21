a625d58c80872ace265318667ebd5d6a1c902d3f
Nic Stelter <nicholas.stelter@gmail.com>
2022-01-21 10:56:07 -0500

    add product export

---
+++ data/Product.js
@@ -0,0 +1,7 @@
+export const Product = {
+  name: 'Stretch Oxford Shirt',
+  description: 'The look of a classic Oxford with add stretch for comfort.',
+  imgURL: 'https://bonobos-prod-s3.imgix.net/products/234448/original/WOVEN-CASUAL-SHIRT_OXFORD-WOVEN-SHIRT_BSH01759SG1348_1.jpg?1642232733=&auto=format&fit=clip&cs=srgb&w=375&q=75',
+  price: '$89',
+  colors: ['red', 'green', 'yellow', 'blue'],
+}
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { Product } from '../data/Product';
 
 export default function Home() {
+  console.log(Product);
   return (
     <div className={styles.container}>
       <Head>
